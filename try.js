791b7e6de2d14008ee15c2cbe4b71718e26ba5e4
Tanishaadhith <pavimba07@gmail.com>
2023-07-25 17:03:36 +0530

    comit

---
+++ try.js
@@ -0,0 +1,25 @@
+var name="APPLE";
+console.log(`my name is ${name}`);
+alert("Hai");
+let x=parseInt(prompt("Enetr a:"));
+//console.log(x);
+let y=parseInt(prompt("Enter b:"));
+alert(`Sum= ${x+y}`);
+let name1=prompt("Enter your name:");
+alert(`hai ${name1}. How are you?`);
+const person={
+    name:"xyz",
+    age:23,
+}
+var person2= person;
+person2.age=98;
+console.log(person.age);
+console.log(person.age);
+const arr=['a','b','c','d'];
+console.log(arr);
+arr.push("e");
+console.log(arr);
+arr.unshift("f");
+console.log(arr);
+arr.pop();
+console.log(arr);
